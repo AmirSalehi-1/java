6c44a4235751a125f3338da7d095d9f09a2b920c
AmirSalehi <lavie.salehi@gmail.com>
2022-12-29 11:21:14 -0500

    Signed-off-by: AmirSalehi <lavie.salehi@gmail.com>

---
+++ Hello/src/Greeting.java
@@ -0,0 +1,19 @@
+
+public class Greeting {
+	//My name : Amir Salehi
+	//This Class display greeting with my name
+	public class Program {
+		public static void main(String[] args) {
+			String name ="Amir Salehi" ;
+			System.out.println("Welcome to JAVA!");
+			System.out.print("Java says: Hello ");
+			System.out.print(name);
+			System.out.println();//New line
+			System.out.println("Program modified by " + name);
+
+		}
+
+	}
+
+}
+
